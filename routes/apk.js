2da04a16571cfcb262e263eabe570ba6b75077db
Anup Saund <anup.saund@commontime.com>
2017-08-25 00:48:27 +0100

    APK download end point was missing code

--- routes/apk.js
+++ routes/apk.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET apk files. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:apk', function(req, res, next) {
+  var pwd =(path.dirname(require.main.filename || process.mainModule.filename));
+  var filename = path.resolve(appRoot, '/../public/files/') + req.params.apk;
+
+  fs.stat(filename, (err)=>{
+    var readStream = fs.createReadStream(filename);
+
+    readStream.on('open', function() {
+      readStream.pipe(res);
+    });
+
+    readStream.on('error', function(err) {
+      res.end(err);
+    });
+  });
 });
 
 module.exports = router;
